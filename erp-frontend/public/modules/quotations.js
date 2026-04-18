0848c07783456ba98443498349a2ff58ac5c49c4
alanmauri4815 <alanmauri4815@gmail.com>
2026-04-18 14:54:53 -0400

    Actualizando el modulo PUBLIC de cotizaciones que es el que se usa en produccion

--- erp-frontend/public/modules/quotations.js
+++ erp-frontend/public/modules/quotations.js
@@ -210,7 +210,7 @@ window.editQuotation = async (id) => {
         calculation_type: it.calculation_type || 'unit',
         linked_to: it.linked_to || 'general',
         description: it.description,
-        document_type: it.document_type || 'factura',
+        document_type: (it.price_gross === 1) ? 'boleta' : 'factura',
         unit_value_net: it.unit_cost,
         quantity: it.quantity
     }));
@@ -295,7 +295,7 @@ window.saveQuotation = async () => {
                     quantity: it.quantity,
                     unit_cost: it.unit_value_net,
                     total_cost: Math.round(projectTotal),
-                    document_type: it.document_type
+                    price_gross: (it.document_type === 'boleta') ? 1 : 0
                 };
             })
     };
